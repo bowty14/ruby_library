11bf43aaed856bf95fed4d0c2d2d07d5f6fcf43e
Tyler Bowerman <44910152+bowty14@users.noreply.github.com>
2020-05-21 16:18:02 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,10 +4,8 @@ require('./lib/author')
 require('./lib/book')
 require('./lib/consumer')
 require('pry')
-require('pg')
 also_reload('lib/**/*.rb')
 
-DB = PG.connect({:dbname => "library"})
 
 get('/')do
   redirect to('/home')
